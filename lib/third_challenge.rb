be8cecc96e67559e4341bc769cfe4c77364f26e9
excellent-revision-4527 <tjcroley@outlook.com>
2020-02-23 01:59:40 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,11 +2,11 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        :name "Lord Montague"
+        name: "Lord Montague"
         :age "53"
         },
       :matriarch => {
-        :name "Lady Montague"
+        name "Lady Montague"
         :age "54"
         },
       :hero => {
